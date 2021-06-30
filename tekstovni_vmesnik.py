131dc10a508ef2af2b4a8948518cf298eb6c6d21
Matija Kocbek <mkocbek1@gmail.com>
2021-06-30 10:06:38 +0200

    Dopolnil zahtevo za vnos Dodal opozoril za neveljaven vnos Dodal funkcijo za pogon vmesnika

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -14,5 +14,25 @@ def izpis_igre(igra):
 def izpis_konca_igre(igra):
     return f'Igra je končana. Ne moreš več narediti nobene poteze. Dosegel si {igra.steviloTock} točk.'
 
-def zahtevaj_vnos():
+def zahtevaj_smer():
     return input("Smer: ")
+
+def zahtevaj_velikost():
+    return input("Velikost tabele: ")
+
+def izpis_neobstojece_smeri(smer):
+    return f'Ukaz {smer} je neveljaven, saj ne obstaja smer {smer}. Ponovno vnesite smer.'
+
+def pozeni_vmesnik():
+    igra = model.nova_igra(zahtevaj_velikost())
+
+    while True:
+        print(izpis_igre(igra))
+        smer = zahtevaj_smer()
+        stanje = igra.premakni(smer)
+
+        if stanje == model.KONEC_IGRE:
+            print(izpis_konca_igre(igra))
+            break
+        elif stanje == model.NEOBSTOJECA_SMER:
+            print(izpis_neobstojece_smeri(smer))
