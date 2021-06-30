99115c6c74c7807819a3b2345370d30893a4b131
Matija Kocbek <mkocbek1@gmail.com>
2021-06-30 09:55:46 +0200

    Dodal izpis konca igre Dodal zahtevo za vnos

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -10,3 +10,9 @@ def izpis_igre(igra):
     
     izpis += igra.velikost * '-' + '\n'
     return izpis
+
+def izpis_konca_igre(igra):
+    return f'Igra je končana. Ne moreš več narediti nobene poteze. Dosegel si {igra.steviloTock} točk.'
+
+def zahtevaj_vnos():
+    return input("Smer: ")
