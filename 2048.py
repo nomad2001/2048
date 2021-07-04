d4b4601f5d5b81990a6d9af9cc81322777a508ba
Matija Kocbek <mkocbek1@gmail.com>
2021-07-04 09:31:40 +0200

    Popravljena registracija

--- 2048.py
+++ 2048.py
@@ -65,7 +65,7 @@ def registracija():
         uporabnik = model.Uporabnik.registracija(uporabnisko_ime, geslo1)
         trenutni_uporabniki = model.Uporabniki.preberi_iz_datoteke(model.DATOTEKA_ZA_UPORABNIKE)
         trenutni_uporabniki.uporabniki[uporabnik.uporabnisko_ime] = uporabnik
-        trenutni_uporabniki.model.Uporabniki.zapisi_v_datoteko(model.DATOTEKA_ZA_UPORABNIKE)
+        trenutni_uporabniki.zapisi_v_datoteko(model.DATOTEKA_ZA_UPORABNIKE)
         bottle.redirect("/prijava/")
     except ValueError:
         return bottle.template("registracija.html", napaka = 2)
